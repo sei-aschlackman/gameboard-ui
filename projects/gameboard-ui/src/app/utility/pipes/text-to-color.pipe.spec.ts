383cc12afb779a2e5f0e5b208b03423e67c55c75
Ben Stein <bsstein@sei.cmu.edu>
2023-01-09 11:14:54 -0500

    Delete unused spec files.

--- projects/gameboard-ui/src/app/utility/pipes/text-to-color.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TextToColorPipe } from './text-to-color.pipe';
-
-describe('TextToColorPipe', () => {
-  it('create an instance', () => {
-    const pipe = new TextToColorPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
